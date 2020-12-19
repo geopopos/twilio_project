0ea27e0b2227888c56d85147dad37d7f0005c048
Geopopos <groros2@icloud.com>
2020-12-19 16:24:40 -0500

    added message validation to twilio sms endpoint

--- app.py
+++ app.py
@@ -1,15 +1,43 @@
-from flask import Flask, request, redirect
+import os
+
+from flask import Flask, abort, current_app, request, redirect
+from functools import wraps
 from twilio.twiml.messaging_response import MessagingResponse
+from twilio.request_validator import RequestValidator
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+
+def validate_twilio_request(f):
+    """Validates that incoming requests genuinely originated from Twilio"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # Create an instance of the RequestValidator class
+        validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN'))
+
+        # Validate the request using its URL, POST data,
+        # and X-TWILIO-SIGNATURE header
+        request_valid = validator.validate(
+            request.url,
+            request.form,
+            request.headers.get('X-TWILIO-SIGNATURE', ''))
+
+        # Continue processing the request if it's valid (or if DEBUG is True)
+        # and return a 403 error if it's not
+        if request_valid or current_app.debug:
+            return f(*args, **kwargs)
+        else:
+            return abort(403)
+    return decorated_function
+
 
 @app.route("/")
 def hello():
     return "Hello World"
 
 @app.route("/sms", methods=['GET', 'POST'])
+@validate_twilio_request
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Start our TwiML response
